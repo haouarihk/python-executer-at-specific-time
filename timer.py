9e070fd4799ef9b496d288093054031b81ccd2d2
Ali Hashemi <60406325+MR-AliHaashemi@users.noreply.github.com>
2020-09-24 06:27:53 +0330

    New version of timer
    I changed lots of things inside of that, I think it's cleaner and may work better.

--- timer.py
+++ timer.py
@@ -1,21 +1,28 @@
-
+import os
+import time
 from datetime import datetime
+
 
 file_path = "file/path.py"
 h = 4 # Witch hour
-m = 0 # Witch minute
+m = 30 # Witch minute
 s = 0 # Witch second
 # ex: at 4:30:00 am 
 
-once = 0 # don't change that
-def executer():
-    exec(open(file_path).read())   
-while True:
-    if h == datetime.now().hour:
-        if m == datetime.now().minute:
-            if s == datetime.now().second:
-                if once==0:
-                    once = 1
-                    executer()
-                else:
-                    once = 0
+
+def main():
+    while True:
+        local_time = datetime.now()
+
+        if h == local_time.hour:
+            if m == local_time.minute:
+                if s == local_time.second:
+                    # Execute file with python command
+                    # Note: Maybe you need to change "python"
+                    # to "python3"
+                    os.system(f"python {file_path}")
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
